1ec3978e4bc3809131878d92393627a88f9ac266
Daniel Parsons <danielparsons3133382@gmail.com>
2017-06-01 14:44:41 +1000

    First commit with Sass configured

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Newspapers?</h2>
+        </div>
+        <p className="App-intro">
+          Digitalised newspaper repository
+        </p>
+        <ul>
+          <li>Title 1</li>
+          <li>Title 2</li>
+          <li>Title 3</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
