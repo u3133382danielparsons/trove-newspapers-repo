fb12667b7a7a87030cfb56dcba21cb2e15866dc3
Daniel Parsons <danielparsons3133382@gmail.com>
2017-06-04 17:14:23 +1000

    Added input box and shortlist components

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import data from './data/data';
-import 'normalize.css';
 import './styles/index.css';
 
 ReactDOM.render(<App data={data} />, document.getElementById('root'));
